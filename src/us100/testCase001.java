58b3f06ff3f5d312f37deb59095bf92010767efa
AnastasiaSmorodin <anastasiasmorodin@gmail.com>
2022-01-11 10:21:16 -0600

    created 2 class

--- src/us100/testCase001.java
+++ src/us100/testCase001.java
@@ -3,5 +3,6 @@ package us100;
 public class testCase001 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("push changes");
     }
 }
