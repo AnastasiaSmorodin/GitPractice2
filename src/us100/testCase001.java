7aa8c6caff7e4922cb38b581239793f57e7664f6
AnastasiaSmorodin <anastasiasmorodin@gmail.com>
2022-01-11 10:25:14 -0600

    gitignore file

--- src/us100/testCase001.java
+++ src/us100/testCase001.java
@@ -4,5 +4,6 @@ public class testCase001 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("push changes");
+        System.out.println("doesnt push");
     }
 }
